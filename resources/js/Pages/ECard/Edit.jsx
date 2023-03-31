8e7d35377d9770e90ca1efa3f5ed553386dba12b
Jake Buceac <jake@3sidedcube.com>
2023-03-31 15:54:13 +0100

    Checking if reloading page after download will work

--- resources/js/Pages/ECard/Edit.jsx
+++ resources/js/Pages/ECard/Edit.jsx
@@ -5,8 +5,6 @@ import { saveAs } from 'file-saver'
 
 export default function Edit({ auth, id, name, image_url, design_state }) {
     function onSave(design_state, image_base_64, filename, width, height) {
-        const base_url = window.location.origin;
-
         axios.put('/e-card/' + id, {
             design_state: JSON.stringify(design_state),
             filename: filename,
@@ -15,12 +13,11 @@ export default function Edit({ auth, id, name, image_url, design_state }) {
         })
             .then(function (response) {
                 saveAs(response.data, filename);
+                name = filename;
             })
             .catch(function (error) {
                 console.log(error);
             });
-
-        return window.location.href = base_url + '/dashboard';
     }
 
     return (
@@ -32,11 +29,6 @@ export default function Edit({ auth, id, name, image_url, design_state }) {
             <div className="w-full sm:max-w-screen-sm bg-white shadow-md overflow-hidden sm:rounded-lg">
                 <div className="sm:px-6 lg:px-8">
                     <div className="px-6 py-4">
-                        <header className="bg-white">
-                            <div className="max-w-7xl mx-auto py-6 text-center">
-                                <h2 className="font-semibold text-xl text-gray-800 leading-tight">{name}</h2>
-                            </div>
-                        </header>
                         <FilerobotImageEditor
                             defaultSavedImageName={name}
                             loadableDesignState={design_state}
