56e65a618a55a05163ba1e954a1bd2f6c6a71278
Jake Buceac <jake@3sidedcube.com>
2023-04-03 12:22:58 +0100

    Updated where so where models are passed to the frontend a resource is used

--- app/Http/Resources/ECardInformationResource.php
+++ app/Http/Resources/ECardInformationResource.php
@@ -19,7 +19,7 @@ class ECardInformationResource extends JsonResource
             'id' => $this->id,
             'attributes' => [
                 'image_url' => $this->image_url,
-                'assets' => $this->assets,
+                'assets' => json_decode($this->assets),
                 'created_at' => $this->created_at,
                 'updated_at' => $this->updated_at,
             ],
