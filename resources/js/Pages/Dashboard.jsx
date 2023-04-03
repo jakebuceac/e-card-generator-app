6107d71883769c6959c56d7d1a27e819db6851e6
Jake Buceac <jake@3sidedcube.com>
2023-04-03 21:22:35 +0100

    Updated some css

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -11,7 +11,7 @@ export default function Dashboard(props) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-5xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg py-4">
                         <div className="py-6 text-center">
                             <h2 className="font-semibold text-xl text-gray-800 leading-tight">Your Recent E-Cards</h2>
