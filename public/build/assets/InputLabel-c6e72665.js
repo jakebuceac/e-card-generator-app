80764d4386cb533caffda2ae6eeb63003ab2df0b
Jake Buceac <jake@3sidedcube.com>
2023-04-03 21:19:33 +0100

    Ran npm run build

---
+++ public/build/assets/InputLabel-c6e72665.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-92ab49ce.js";function o({value:t,className:e="",children:r,...a}){return m("label",{...a,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
